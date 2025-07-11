5244975c55dbf2e54fc67cdc8d75383c387c4ffb
afnan <afnanmedhat.94@gmail.com>
2025-07-12 01:16:49 +0300

    edit2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import WelcomeMessage from './components/WelcomeMessage';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 
     <>
+       <WelcomeMessage />
     <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -30,7 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      
+   
     </>
   )
 }
