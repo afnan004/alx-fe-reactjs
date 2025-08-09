94c0827b5da0070e7056e39ffed5b075f2eda969
afnan <afnanmedhat.94@gmail.com>
2025-08-09 23:57:53 +0300

    task 3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 // src/components/UserProfile.jsx
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-3 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-16 md:my-20 rounded-lg shadow-lg user-profile">
+      <div
+        className="bg-gray-100 p-3 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-16 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-out user-profile"
+      >
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+          className="rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
         />
-        <h1 className="text-base sm:text-lg md:text-xl text-blue-800 my-3 md:my-4 text-center">
+        <h1 className="text-base sm:text-lg md:text-xl text-blue-800 my-3 md:my-4 text-center transition-colors duration-200 ease-out hover:text-blue-500">
           John Doe
         </h1>
         <p className="text-gray-600 text-sm md:text-base text-center">
