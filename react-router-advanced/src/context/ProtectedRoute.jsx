709f834872766ae4c4d483c108521d14d9ae03bd
afnan <afnanmedhat.94@gmail.com>
2025-08-23 18:56:46 +0300

    task2

--- react-router-advanced/src/context/ProtectedRoute.jsx
+++ react-router-advanced/src/context/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
+  // ✅ useAuth is explicitly used here
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
