611b57b7aba990668aa4d6bf5043d9fb750129de
afnan <afnanmedhat.94@gmail.com>
2025-08-23 18:47:15 +0300

    task2 edit1

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,48 +1,50 @@
-import { Routes, Route, Link, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import BlogPost from "./components/BlogPost";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <div>
-      <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/profile">Profile</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
+          <Link to="/">Home</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
 
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:postId" element={<BlogPost />} />
-        <Route path="/login" element={<Login />} />
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:postId" element={<BlogPost />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* Protected route wrapper */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          {/* Nested routes inside Profile */}
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-          <Route index element={<Navigate to="details" replace />} />
-        </Route>
+          {/* Protected + Nested Routes */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+            <Route index element={<Navigate to="details" replace />} />
+          </Route>
 
-        {/* 404 route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
